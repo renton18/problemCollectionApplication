08d3c2c6e1a8aa56697dd40619a0f98bb0e2d848
renton18 <gakituka18@gmail.com>
2019-02-22 11:06:44 +0900

    firstcommit2

--- app/Models/problem.php
+++ app/Models/problem.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class problem extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'm_problem';
+    protected $dates = ['deleted_at'];
 }
