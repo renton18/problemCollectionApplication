f9b445d409875ff3349f487779a78cd625d294c5
renton18 <gakituka18@gmail.com>
2019-02-22 11:06:10 +0900

    firstcommit

---
+++ database/migrations/2019_02_21_132304_problem.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Problem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_problem', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('problemId');
+            $table->dateTime('problemUpdateTime');
+            $table->text('problemTitle');
+            $table->string('problemCategory');
+            $table->text('selectItem1');
+            $table->text('selectItem2')->nullable();
+            $table->text('selectItem3')->nullable();
+            $table->text('selectItem4')->nullable();
+            $table->text('selectItem5')->nullable();
+            $table->text('selectItem6')->nullable();
+            $table->text('memo')->nullable();
+            $table->boolean('yesNoProblemFlag');
+            $table->integer('explainId')->nullable()->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->unique(['problemId', 'problemUpdateTime']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_problem');
+    }
+}
